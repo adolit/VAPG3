34cbeee75bb0a225005fd4375ad44f1c2e9a5604
adolit <adolit@gmail.com>
2021-08-08 01:20:38 +0800

    [2021-08-08](Archie) Updated base app.R, Added global.R

--- 02_shiny/workArea/Archie/app-2.R
+++ 02_shiny/workArea/Archie/app-2.R
@@ -23,144 +23,276 @@ library(tidyverse)
 # bgmap <- raster("data/Geospatial/abila_map.tif")
 # 
 
+
+
+
 # Define UI ----
-ui <- dashboardPage(
-    #Header content
-    dashboardHeader(title = "V-ASK App"),
-    
-    #UI/UX style and customization
-    #skin = "blue",
-    
-    #Sidebar content
-    dashboardSidebar(
-        sidebarMenu(
-            menuItem("Home", tabName = "home", icon = icon("home")),
-            menuItem("Spending", tabName = "spending", icon = icon("comment-dollar")),
-            menuItem("Kinematics", tabName = "kinematics", icon = icon("route")),
-            menuItem("Anomalies", tabName = "anomalies", icon = icon("exclamation-triangle")),
-            menuItem("Relationship", tabName = "relationship", icon = icon("people-arrows")),
-            menuItem("Insights", tabName = "insights", icon = icon("search-location"))
-        )
-    ), #dashboardSidebar
-    
-    dashboardBody(
-        tabItems(
-            tabItem(tabName ="home",
-                    h1("Overview"),
-                    fluidRow(align = "left",
-                             h2("Context + Overview of the App")
-                    )
-            ), #tabItem home
-            
-            tabItem(tabName ="spending",
-                    h1("Credit Card and Loyalty Data Exploratory Data Analysis"),
-                    tabsetPanel(
-                        tabPanel("Credit Card Data",
-                                 fluidRow(
-                                     box(
-                                         title = "Most Popular Locations", width = 12, status = "primary", solidHeader = TRUE,
+ui <- navbarPage("V-ASK App",
+                 position = "static-top",
+                 collapsible = TRUE,
+                 fluid = TRUE, 
+                 theme = bslib::bs_theme(
+                     bootswatch = "sandstone"),
+                 
+                 #Sy---
+                 tabPanel("Home", icon = icon("home"),
+                          fluidPage(
+                              h1("Overview"),
+                              fluidRow(align = "left",
+                                       h2("Context + Overview of the App")
+                                       )
+                              )
+                 ),
+                 
+                 tabPanel("Spending", icon = icon("comment-dollar"),
+                          fluidPage(
+                              tabsetPanel(
+                                  tabPanel("Credit Card Data",
+                                           fluidRow(
+                                               box(
+                                                   title = "Most Popular Locations", width = 12, status = "primary", solidHeader = TRUE,
+                                               )
+                                           )
+                                  ),
+                                  
+                                  tabPanel("Loyalty Data",
+                                           fluidRow(
+                                               box(
+                                                   title = "Most Popular Locations", width = 12, status = "primary", solidHeader = TRUE,
+                                               )
+                                           )
+                                  ),
+                                  
+                                  tabPanel("Credit Card + Loyalty Data",
+                                           fluidRow(
+                                               box(
+                                                   title = "Mapping of Credit Card and Loyalty Number", width = 12, status = "primary", solidHeader = TRUE,
+                                               )
+                                           )
+                                  )
+                              )
+                          )
+                 ),
+                 
+                 #ke ----
+                 navbarMenu("Kinematics", icon = icon("route"),
+                            tabPanel("GPS Movement",
+                                     fluidRow(
+                                         box(
+                                             title = "Interactive Map", width = 12, status = "primary", solidHeader = TRUE,
+                                         )
                                      )
-                                 )
-                        ),
-                        
-                        tabPanel("Loyalty Data",
-                                 fluidRow(
-                                     box(
-                                         title = "Most Popular Locations", width = 12, status = "primary", solidHeader = TRUE,
+                            ),
+                            
+                            tabPanel("Owners Identification",
+                                     fluidRow(
+                                         box(
+                                             title = "Credit Card and Loyalty Number Owners", width = 12, status = "primary", solidHeader = TRUE,
+                                         )
                                      )
-                                 )
-                        ),
-                        
-                        tabPanel("Credit Card + Loyalty Data",
-                                 fluidRow(
-                                     box(
-                                         title = "Mapping of Credit Card and Loyalty Number", width = 12, status = "primary", solidHeader = TRUE,
-                                     )
-                                 )
+                            )
+                 ),
+                 
+                 tabPanel("Anomalies", icon = icon("exclamation-triangle"),
+                     fluidPage(
+                         tabsetPanel(                         
+                             tabPanel("Financial Anomalies",
+                                      fluidRow(
+                                          box(
+                                              title = "Anomalies based on Credit Card and Loyalty Data", width = 12, status = "primary", solidHeader = TRUE,
+                                          )
+                                      )
+                             ),
+                             
+                             tabPanel("Movement Anomalies",
+                                      fluidRow(
+                                          box(
+                                              title = "Anomalies based on GPS Data", width = 12, status = "primary", solidHeader = TRUE,
+                                          )
+                                      )
+                             )
+                         )
+                     )
+                 ),
+                 
+                 #
+                 navbarMenu("Relationship", icon = icon("people-arrows"),
+                         tabPanel("Spending Habits",
+                                  fluidRow(
+                                      box(
+                                          title = "Relationship based on Credit Card and Loyalty Data", width = 12, status = "primary", solidHeader = TRUE,
+                                      )
+                                  )
+                         ),
+                         
+                         tabPanel("Movement",
+                                  fluidRow(
+                                      box(
+                                          title = "Relationship based on GPS Data", width = 12, status = "primary", solidHeader = TRUE,
+                                      )
+                                  )
+                         )
+                 ),
+                 
+                 tabPanel("Insights",icon = icon("search-location"),
+                    fluidPage(
+                        fluidRow(align = "left",
+                                 h2("Identification of Suspicious Activities")
                         )
                     )
-            ), #tabItem spending
-            
-            tabItem(tabName ="kinematics",
-                    h1("Pattern of Life Kinenamatics"),
-                    tabsetPanel(
-                        tabPanel("GPS Movement",
-                                 fluidRow(
-                                     box(
-                                         title = "Interactive Map", width = 12, status = "primary", solidHeader = TRUE,
-                                     )
-                                 )
-                        ),
-                        
-                        tabPanel("Owners Identification",
-                                 fluidRow(
-                                     box(
-                                         title = "Credit Card and Loyalty Number Owners", width = 12, status = "primary", solidHeader = TRUE,
-                                     )
-                                 )
-                        )
-                    )
-            ), #tabItem kinematics
-            
-            
-            tabItem(tabName ="anomalies",
-                    h1("Anomaly Diagnostics"),
-                    tabsetPanel(
-                        tabPanel("Financial Anomalies",
-                                 fluidRow(
-                                     box(
-                                         title = "Anomalies based on Credit Card and Loyalty Data", width = 12, status = "primary", solidHeader = TRUE,
-                                     )
-                                 )
-                        ),
-                        
-                        tabPanel("Movement Anomalies",
-                                 fluidRow(
-                                     box(
-                                         title = "Anomalies based on GPS Data", width = 12, status = "primary", solidHeader = TRUE,
-                                     )
-                                 )
-                        )
-                    )
-            ), #tabItem anomalies
-            
-            tabItem(tabName ="relationship",
-                    h1("GASTech Personnel Relationships"),
-                    tabsetPanel(
-                        tabPanel("Spending Habits",
-                                 fluidRow(
-                                     box(
-                                         title = "Relationship based on Credit Card and Loyalty Data", width = 12, status = "primary", solidHeader = TRUE,
-                                     )
-                                 )
-                        ),
-                        
-                        tabPanel("Movement",
-                                 fluidRow(
-                                     box(
-                                         title = "Relationship based on GPS Data", width = 12, status = "primary", solidHeader = TRUE,
-                                     )
-                                 )
-                        )
-                    )
-            ), #tabItem relationship
-            
-            
-            tabItem(tabName ="insights",
-                    h1("Suspicious Activities"),
-                    
-                    fluidRow(align = "left",
-                             h2("Identification of Suspicious Activities")
-                    )
-            ) #tabItem insights
-        ) #tabItems
-    ) #dashboardBody
-    
+                 )
 ) #ui
+                 
+                 
+                         
+                 
+
+    
+    
+    
+    
+    
+    # dashboardPage(
+    # #Header content
+    # dashboardHeader(title = "V-ASK App"),
+    # 
+    # #UI/UX style and customization
+    # #skin = "blue",
+    # 
+    # #Sidebar content
+    # dashboardSidebar(
+    #     sidebarMenu(
+    #         menuItem("Home", tabName = "home", icon = icon("home")),
+    #         menuItem("Spending", tabName = "spending", icon = icon("comment-dollar")),
+    #         menuItem("Kinematics", tabName = "kinematics", icon = icon("route")),
+    #         menuItem("Anomalies", tabName = "anomalies", icon = icon("exclamation-triangle")),
+    #         menuItem("Relationship", tabName = "relationship", icon = icon("people-arrows")),
+    #         menuItem("Insights", tabName = "insights", icon = icon("search-location"))
+    #     )
+    # ), #dashboardSidebar
+    # 
+    # dashboardBody(
+    #     tabItems(
+    #         tabItem(tabName ="home",
+    #                 h1("Overview"),
+    #                 fluidRow(align = "left",
+    #                          h2("Context + Overview of the App")
+    #                 )
+    #         ), #tabItem home
+    #         
+    #         tabItem(tabName ="spending",
+    #                 h1("Credit Card and Loyalty Data Exploratory Data Analysis"),
+    #                 tabsetPanel(
+    #                     tabPanel("Credit Card Data",
+    #                              fluidRow(
+    #                                  box(
+    #                                      title = "Most Popular Locations", width = 12, status = "primary", solidHeader = TRUE,
+    #                                  )
+    #                              )
+    #                     ),
+    #                     
+    #                     tabPanel("Loyalty Data",
+    #                              fluidRow(
+    #                                  box(
+    #                                      title = "Most Popular Locations", width = 12, status = "primary", solidHeader = TRUE,
+    #                                  )
+    #                              )
+    #                     ),
+    #                     
+    #                     tabPanel("Credit Card + Loyalty Data",
+    #                              fluidRow(
+    #                                  box(
+    #                                      title = "Mapping of Credit Card and Loyalty Number", width = 12, status = "primary", solidHeader = TRUE,
+    #                                  )
+    #                              )
+    #                     )
+    #                 )
+    #         ), #tabItem spending
+    #         
+    #         tabItem(tabName ="kinematics",
+    #                 h1("Pattern of Life Kinenamatics"),
+    #                 tabsetPanel(
+    #                     tabPanel("GPS Movement",
+    #                              fluidRow(
+    #                                  box(
+    #                                      title = "Interactive Map", width = 12, status = "primary", solidHeader = TRUE,
+    #                                  )
+    #                              )
+    #                     ),
+    #                     
+    #                     tabPanel("Owners Identification",
+    #                              fluidRow(
+    #                                  box(
+    #                                      title = "Credit Card and Loyalty Number Owners", width = 12, status = "primary", solidHeader = TRUE,
+    #                                  )
+    #                              )
+    #                     )
+    #                 )
+    #         ), #tabItem kinematics
+    #         
+    #         
+    #         tabItem(tabName ="anomalies",
+    #                 h1("Anomaly Diagnostics"),
+    #                 tabsetPanel(
+    #                     tabPanel("Financial Anomalies",
+    #                              fluidRow(
+    #                                  box(
+    #                                      title = "Anomalies based on Credit Card and Loyalty Data", width = 12, status = "primary", solidHeader = TRUE,
+    #                                  )
+    #                              )
+    #                     ),
+    #                     
+    #                     tabPanel("Movement Anomalies",
+    #                              fluidRow(
+    #                                  box(
+    #                                      title = "Anomalies based on GPS Data", width = 12, status = "primary", solidHeader = TRUE,
+    #                                  )
+    #                              )
+    #                     )
+    #                 )
+    #         ), #tabItem anomalies
+    #         
+    #         tabItem(tabName ="relationship",
+    #                 h1("GASTech Personnel Relationships"),
+    #                 tabsetPanel(
+    #                     tabPanel("Spending Habits",
+    #                              fluidRow(
+    #                                  box(
+    #                                      title = "Relationship based on Credit Card and Loyalty Data", width = 12, status = "primary", solidHeader = TRUE,
+    #                                  )
+    #                              )
+    #                     ),
+    #                     
+    #                     tabPanel("Movement",
+    #                              fluidRow(
+    #                                  box(
+    #                                      title = "Relationship based on GPS Data", width = 12, status = "primary", solidHeader = TRUE,
+    #                                  )
+    #                              )
+    #                     )
+    #                 )
+    #         ), #tabItem relationship
+    #         
+    #         
+    #         tabItem(tabName ="insights",
+    #                 h1("Suspicious Activities"),
+    #                 
+    #                 fluidRow(align = "left",
+    #                          h2("Identification of Suspicious Activities")
+    #                 )
+    #         ) #tabItem insights
+    #     ) #tabItems
+    # ) #dashboardBody
+    # 
+
     
 
 # Define server logic ----
 server <- function(input, output) {
+    
+    
+    
+    
 }
 
 # Run the application 
