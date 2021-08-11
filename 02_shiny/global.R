5c07dc37daffdb333886fdd7cc751b832ae2eb81
adolit <adolit@gmail.com>
2021-08-11 10:38:31 +0800

    [2021-08-11](Archie) Updated data/ and global.R, read from rds instead of raw data

--- 02_shiny/global.R
+++ 02_shiny/global.R
@@ -17,10 +17,10 @@ library(tidyverse)
 
 
 #import csv data ----
-df_cc <- read_csv("data/aspatial/cc_data.csv")
-df_loyalty <- read_csv("data/aspatial/loyalty_data.csv")
-df_cars <- read_csv("data/aspatial/car-assignments.csv")
-df_gps <- read_csv("data/aspatial/gps.csv")
+# df_cc <- read_csv("data/aspatial/cc_data.csv")
+# df_loyalty <- read_csv("data/aspatial/loyalty_data.csv")
+# df_cars <- read_csv("data/aspatial/car-assignments.csv")
+# df_gps <- read_csv("data/aspatial/gps.csv")
 
 
 #import georeference tif file
@@ -29,101 +29,135 @@ bgmap <- raster("data/Geospatial/abila_map.tif")
 
 #data preparation ----
 #clean credit card data
-df_cc <- df_cc %>%
-  mutate(location = ifelse(str_detect(location, "Katerina"), "Katerina's Cafe", location))
+# df_cc <- df_cc %>%
+#   mutate(location = ifelse(str_detect(location, "Katerina"), "Katerina's Cafe", location))
+# 
+# df_cc$date <- date_time_parse(df_cc$timestamp,
+#                               zone = "",
+#                               format = "%m/%d/%Y")
+# 
+# df_cc$day <- wday(df_cc$date,
+#                   label = TRUE,
+#                   abbr = TRUE)
+# 
+# df_cc$timestamp <- date_time_parse(df_cc$timestamp,
+#                                    zone = "",
+#                                    format = "%m/%d/%Y %H:%M")
+# 
+# df_cc$hour <- get_hour(df_cc$timestamp)
+# 
+# 
+# write_rds(df_cc, "data/aspatial/df_cc.rds")
 
-df_cc$date <- date_time_parse(df_cc$timestamp,
-                              zone = "",
-                              format = "%m/%d/%Y")
+df_cc <- read_rds("data/aspatial/df_cc.rds")
 
-df_cc$day <- wday(df_cc$date,
-                  label = TRUE,
-                  abbr = TRUE)
 
-df_cc$timestamp <- date_time_parse(df_cc$timestamp,
-                                   zone = "",
-                                   format = "%m/%d/%Y %H:%M")
-
-df_cc$hour <- get_hour(df_cc$timestamp)
 
 #clean loyalty data
-df_loyalty <- df_loyalty %>%
-  mutate(location = ifelse(str_detect(location, "Katerina"), "Katerina's Cafe", location))
+# df_loyalty <- df_loyalty %>%
+#   mutate(location = ifelse(str_detect(location, "Katerina"), "Katerina's Cafe", location))
+# 
+# df_loyalty$date <- date_time_parse(df_loyalty$timestamp,
+#                                    zone = "",
+#                                    format = "%m/%d/%Y")
+# 
+# df_loyalty$timestamp <- date_time_parse(df_loyalty$timestamp,
+#                                         zone = "",
+#                                         format = "%m/%d/%Y")
+# 
+# df_loyalty$day <- wday(df_loyalty$timestamp,
+#                        label = TRUE,
+#                        abbr = TRUE)
+# 
+# write_rds(df_loyalty, "data/aspatial/df_loyalty.rds")
 
-df_loyalty$date <- date_time_parse(df_loyalty$timestamp,
-                                   zone = "",
-                                   format = "%m/%d/%Y")
-
-df_loyalty$timestamp <- date_time_parse(df_loyalty$timestamp,
-                                        zone = "",
-                                        format = "%m/%d/%Y")
-
-df_loyalty$day <- wday(df_loyalty$timestamp,
-                       label = TRUE,
-                       abbr = TRUE)
+df_loyalty <-  read_rds("data/aspatial/df_loyalty.rds")
 
 
 #clean car assignment
-df_cars <- df_cars %>%
-  #concatenate first and last name
-  mutate(FullName = paste(FirstName, LastName, sep = " ")) %>%
-  rename(Department = CurrentEmploymentType) %>%
-  rename(Title = CurrentEmploymentTitle)
+# df_cars <- df_cars %>%
+#   #concatenate first and last name
+#   mutate(FullName = paste(FirstName, LastName, sep = " ")) %>%
+#   rename(Department = CurrentEmploymentType) %>%
+#   rename(Title = CurrentEmploymentTitle)
+# 
+# df_cars$CarID <- as_factor(df_cars$CarID)
+# 
+# write_rds(df_cars, "data/aspatial/df_cars.rds")
 
-df_cars$CarID <- as_factor(df_cars$CarID)
+df_cars <- read_rds("data/aspatial/df_cars.rds")
 
 
 #clean gps data
-df_gps <- df_gps %>%
-  rename(timestamp = Timestamp) %>%
-  rename(CarID = id)
+# df_gps <- df_gps %>%
+#   rename(timestamp = Timestamp) %>%
+#   rename(CarID = id)
+# 
+# df_gps$date <- date_time_parse(df_gps$timestamp,
+#                                zone = "",
+#                                format = "%m/%d/%Y")
+# 
+# df_gps$day <- as.factor(wday(df_gps$date,
+#                              label = TRUE,
+#                              abbr = TRUE))
+# 
+# df_gps$timestamp <- date_time_parse(df_gps$timestamp,
+#                                     zone = "",
+#                                     format = "%m/%d/%Y %H:%M:%S")
+# 
+# df_gps$hour <- get_hour(df_gps$timestamp)
+# 
+# df_gps$CarID <- as_factor(df_gps$CarID)
+# 
+# write_rds(df_cars, "data/aspatial/df_gps.rds")
 
-df_gps$date <- date_time_parse(df_gps$timestamp,
-                               zone = "",
-                               format = "%m/%d/%Y")
-
-df_gps$day <- as.factor(wday(df_gps$date,
-                             label = TRUE,
-                             abbr = TRUE))
-
-df_gps$timestamp <- date_time_parse(df_gps$timestamp,
-                                    zone = "",
-                                    format = "%m/%d/%Y %H:%M:%S")
-
-df_gps$hour <- get_hour(df_gps$timestamp)
-
-df_gps$CarID <- as_factor(df_gps$CarID)
+df_gps <- read_rds("data/aspatial/df_gps.rds")
 
 
 #data joining ----
 #financial data
-df_cc_loyalty <- full_join(df_cc %>% select(-c("day")),
-                           df_loyalty %>% select(-c("day","timestamp")), 
-                           by = c("date" = "date", 
-                                  "location" = "location", 
-                                  "price" = "price"))
+# df_cc_loyalty <- full_join(df_cc %>% select(-c("day")),
+#                            df_loyalty %>% select(-c("day","timestamp")), 
+#                            by = c("date" = "date", 
+#                                   "location" = "location", 
+#                                   "price" = "price"))
+# 
+# df_cc_loyalty$day <- wday(df_cc_loyalty$date,
+#                           label = TRUE,
+#                           abbr = TRUE)
+# 
+# #rearrange columns
+# df_cc_loyalty <- df_cc_loyalty %>%
+#   select("timestamp", "date", "day", "hour", "location", "price", "last4ccnum", "loyaltynum")
+# 
+# 
+# write_rds(df_cars, "data/aspatial/df_cc_loyalty.rds")
 
-df_cc_loyalty$day <- wday(df_cc_loyalty$date,
-                          label = TRUE,
-                          abbr = TRUE)
-
-#rearrange columns
-df_cc_loyalty <- df_cc_loyalty %>%
-  select("timestamp", "date", "day", "hour", "location", "price", "last4ccnum", "loyaltynum")
-
+df_cc_loyalty <- read_rds("data/aspatial/df_cc_loyalty.rds")
 
 #geospatial data
-df_car_gps <- left_join(df_gps, 
-                        df_cars %>% select(-c("FirstName", "LastName")),
-                        by = "CarID")
+# df_car_gps <- left_join(df_gps, 
+#                         df_cars %>% select(-c("FirstName", "LastName")),
+#                         by = "CarID")
+# 
+# write_rds(df_cars, "data/aspatial/df_car_gps.rds")
+# 
+# sf_gps <- st_as_sf(df_gps,
+#                    coords = c("long", "lat"),
+#                    crs = 4326)
+# 
+# write_rds(sf_gps, "data/Geospatial/sf_gps.rds")
 
-sf_gps <- st_as_sf(df_gps,
-                   coords = c("long", "lat"),
-                   crs = 4326)
+sf_gps <- read_rds("data/Geospatial/sf_gps.rds")
 
-sf_car_gps <- left_join(sf_gps, 
-                        df_cars %>% select(-c("FirstName", "LastName")),
-                        by = "CarID")
+# 
+# sf_car_gps <- left_join(sf_gps, 
+#                         df_cars %>% select(-c("FirstName", "LastName")),
+#                         by = "CarID")
+# 
+# write_rds(sf_gps, "data/Geospatial/sf_car_gps.rds")
+
+sf_car_gps <- read_rds("data/Geospatial/sf_car_gps.rds")
 
 
 #colors ----
